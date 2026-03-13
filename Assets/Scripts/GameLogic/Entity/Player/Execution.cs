121d6c347f7e50a7caa79b2c965af31bbcce051c
minding2796 <minding2796@gmail.com>
2026-03-13 13:10:10 +0900

    feat: Execution Line Improve

--- Assets/Scripts/GameLogic/Entity/Player/Execution.cs
+++ Assets/Scripts/GameLogic/Entity/Player/Execution.cs
@@ -1,20 +1,22 @@
+using System.Linq;
 using UnityEngine;
 
 namespace GameLogic.Entity.Player
 {
     public class Execution : MonoBehaviour
     {
-        public GameObject Target { get; private set; }
+        public Enemy[] Target { get; private set; }
         public LineRenderer executionLine;
 
         public void FixedUpdate()
         {
             Target = GetNearestExecutionTarget();
-            executionLine.enabled = Target;
-            if (!Target) return;
-            var direction = Target.transform.position - transform.position;
+            executionLine.enabled = Target.Length > 0;
+            if (Target.Length <= 0) return;
+            var direction = Target[0].transform.position - transform.position;
+            executionLine.positionCount = Target.Length + 1;
             executionLine.SetPosition(0, transform.position + direction.normalized * 0.5f);
-            executionLine.SetPosition(1, Target.transform.position);
+            for (var i = 0; i < Target.Length; i++) executionLine.SetPosition(i + 1, Target[i].transform.position);
         }
 
         public void Execute()
@@ -22,10 +24,10 @@ namespace GameLogic.Entity.Player
             
         }
 
-        private GameObject GetNearestExecutionTarget()
+        private Enemy[] GetNearestExecutionTarget()
         {
             var e = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
-            return e[0].gameObject;
+            return e.Where(x => x.IsExecutable).OrderBy(x => Vector3.Distance(x.transform.position, transform.position)).ToArray();
         }
     }
 }
