2df070674c1b4575e1d2c005e9c62ac8e0bc193e
minding2796 <minding2796@gmail.com>
2026-03-17 09:50:29 +0900

    feat: Execution Test

--- Assets/Scripts/GameLogic/Entity/Player/Execution.cs
+++ Assets/Scripts/GameLogic/Entity/Player/Execution.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GameLogic.Entity.Player
 {
@@ -19,9 +20,31 @@ namespace GameLogic.Entity.Player
             for (var i = 0; i < Target.Length; i++) executionLine.SetPosition(i + 1, Target[i].transform.position);
         }
 
+#if UNITY_EDITOR // 테스트용 코드
+        public void OnAttack(InputValue value)
+        {
+            if (value.isPressed) Execute();
+        }
+#endif
+        
         public void Execute()
         {
-            
+            switch (Target.Length)
+            {
+                case 0:
+                    return;
+                case 1:
+                    var o = Target[0];
+                    var dir = o.transform.position - transform.position;
+                    transform.position = o.transform.position + dir.normalized;
+                    Destroy(o.gameObject); // 나중에 비활성화 코드로 바꿀 것
+                    break;
+                default:
+                    var d = Target[^1].transform.position - Target[^2].transform.position;
+                    transform.position = Target[^1].transform.position + d.normalized;
+                    foreach (var i in Target) Destroy(i.gameObject); // 나중에 비활성화 코드로 바꿀 것
+                    break;
+            }
         }
 
         private Enemy[] GetNearestExecutionTarget()
